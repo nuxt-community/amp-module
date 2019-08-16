a0b27fa6b18212986f01c18ed9f2d6ce7fc00da4
Ahad Birang <farnabaz@gmail.com>
2019-08-16 16:52:17 +0430

    chore: add example for custom layout

---
+++ example/layouts/custom.amp.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <div>This is AMP custom layout</div>
+    <Nuxt />
+  </div>
+</template>
---
+++ example/pages/custom-layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Custom Layout
+  </div>
+</template>
+
+<script>
+export default {
+  amp: 'only',
+  layout () {
+    return 'custom'
+  }
+}
+</script>
